5e8760eaa8af22951531d2afec9b5be097b0afee
Nathan Amick <nathan.amick@lifeomic.com>
2023-02-01 15:54:01 -0500

    testing..

--- peer_1.mjs
+++ peer_1.mjs
@@ -1,35 +1,48 @@
-import goodbye from "graceful-goodbye";
-import Hyperswarm from "hyperswarm";
-import Corestore from "corestore";
-import Hyperbee from "hyperbee";
-import b4a from "b4a";
+import goodbye from 'graceful-goodbye'
+import Hyperswarm from 'hyperswarm'
+import Corestore from 'corestore'
+import Hyperbee from 'hyperbee'
+import b4a from 'b4a'
 
-const corestore = new Corestore("./writer-storage");
+const corestore = new Corestore('./writer-storage')
 
-const hyperswarm = new Hyperswarm();
-goodbye(() => hyperswarm.destroy());
-hyperswarm.on("connection", (conn) => {
+const hyperswarm = new Hyperswarm()
+goodbye(() => hyperswarm.destroy())
+hyperswarm.on('connection', (conn) => {
   console.log('connection!!!')
-  
+
   return corestore.replicate(conn)
-});
+})
 
-const hypercore1 = corestore.get({ name: "peerpass-core" });
+const hypercore1 = corestore.get({ name: 'peerpass-core' })
+
 const bee = new Hyperbee(hypercore1, {
-  keyEncoding: "utf-8",
-  valueEncoding: "utf-8",
-});
+  keyEncoding: 'utf-8',
+  valueEncoding: 'utf-8',
+})
 
-await hypercore1.ready();
-hyperswarm.join(hypercore1.discoveryKey);
+await hypercore1.ready()
+hyperswarm.join(hypercore1.discoveryKey)
 
-await bee.put("google", "test_value");
-await bee.put("amazon", "test_value");
+// await bee.put('google', 'test_value')
+// await bee.put('amazon', 'test_value')
+// await bee.put('jazz', 'test_value')
+// await bee.put('jones', 'test_value')
+
+// Do a single Hyperbee.get for every line of stdin data
+// Each `get` will only download the blocks necessary to satisfy the query
+process.stdin.setEncoding('utf-8')
+process.stdin.on('data', async (data) => {
+  const [key, value] = data.split(': ')
+  await bee.put(key, value)
+
+  console.log(`added ${key}: ${value}`)
+})
 
 console.log({
   // core: hypercore1,
   // coreKey: hypercore1.key,
   // coreKeyPair: hypercore1.keyPair,
   // coreDiscoveryKey: hypercore1.discoveryKey,
-  charleyHexDiscoveryKey: b4a.toString(hypercore1.key, "hex"),
-});
+  charleyHexDiscoveryKey: b4a.toString(hypercore1.key, 'hex'),
+})
--- peer_2.mjs
+++ peer_2.mjs
@@ -1,32 +1,82 @@
-import Hyperswarm from "hyperswarm";
-import Corestore from "corestore";
-import Hyperbee from "hyperbee";
-import goodbye from "graceful-goodbye";
-import b4a from "b4a";
+import Hyperswarm from 'hyperswarm'
+import Corestore from 'corestore'
+import Hyperbee from 'hyperbee'
+import goodbye from 'graceful-goodbye'
+import b4a from 'b4a'
+import { Node } from 'hyperbee/lib/messages.js'
 
-// hyperbee
-const store = new Corestore("./reader-storage");
+const charleyHexDiscoveryKey = 'a03d71c5a85dfe7fd4c6d810b5f1f28884acc62685784ad3dad626ee558db71c'
 
-const swarm = new Hyperswarm();
-goodbye(() => swarm.destroy());
-swarm.on("connection", (conn) => store.replicate(conn));
+// create a corestore instance with the given location
+const corestore = new Corestore('./reader-storage')
 
-const hypercore1 = store.get({ key: b4a.from(process.argv[2], "hex") });
-const bee = new Hyperbee(hypercore1, {
-  keyEncoding: "utf-8",
-  valueEncoding: "utf-8",
-});
+const hyperswarm = new Hyperswarm()
+goodbye(() => hyperswarm.destroy())
 
-await hypercore1.ready();
-swarm.join(hypercore1.discoveryKey);
+// replication of the corestore instance on connection with other peers
+hyperswarm.on('connection', (conn) => corestore.replicate(conn))
 
-const resp = await bee.get("test_key");
+// creation of Hypercore instance (if not already created)
+const hypercore = corestore.get({ key: b4a.from(charleyHexDiscoveryKey, 'hex') })
+await hypercore.ready()
 
-console.log({
-  // resp,
-  // core: hypercore1,
-  // coreKey: hypercore1.key,
-  // coreKeyPair: hypercore1.keyPair,
-  // coreDiscoveryKey: hypercore1.discoveryKey,
-  charleyHexDiscoveryKey: b4a.toString(hypercore1.key, "hex"),
-});
+// handshake with peers, but don't wait
+const foundPeers = corestore.findingPeers()
+hyperswarm.join(hypercore.discoveryKey)
+hyperswarm.flush().then(() => foundPeers())
+
+// update the meta-data information of the hypercore instance
+await hypercore.update()
+
+const hyperbee = new Hyperbee(hypercore, {
+  keyEncoding: 'utf-8',
+  valueEncoding: 'utf-8',
+})
+
+const logReadStream = async () => {
+  const db = {}
+  for await (const { key, value } of hyperbee.createReadStream()) {
+    db[key?.trim()] = value?.trim()
+  }
+  console.log({ db })
+}
+
+let version = hyperbee.version
+console.log(hyperbee.version)
+setInterval(() => {
+  if (hyperbee.version > version) {
+    console.log(hyperbee.version)
+    version = hyperbee.version
+
+    logReadStream()
+
+    // const snapshot = hyperbee.snapshot()
+    // console.log({ snapshot })
+  }
+}, 500)
+
+// process.stdin.setEncoding('utf-8')
+// process.stdin.on('data', async (data) => {
+//   // const dynamic = await bee.get('dynamic')
+
+//   // console.log({ dynamic })
+
+//   const seq = hypercore1.length - 1
+//   const lastBlock = await hypercore1.get(hypercore1.length - 1)
+//   const { index, key, value } = Node.decode(lastBlock)
+//   const dIndex = Node.decode(index)
+//   // const dKey = Node.decode(key)
+//   // const dValue = Node.decode(value)
+
+//   // ​
+//   // // print the information about the last block or the latest block of the hypercore instance
+//   console.log({
+//     seq,
+//     index,
+//     key,
+//     value,
+//     dIndex,
+//     // dKey,
+//     // dValue,
+//   })
+// })
