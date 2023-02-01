94995a6ebd304e42fbca4ca9f2414b20af5666f5
Nathan Amick <nathan.amick@lifeomic.com>
2023-02-01 17:14:15 -0500

    Improve commenting

--- peer_2.mjs
+++ peer_2.mjs
@@ -5,43 +5,44 @@ import goodbye from 'graceful-goodbye'
 import b4a from 'b4a'
 import { Node } from 'hyperbee/lib/messages.js'
 
-const charleyHexDiscoveryKey = 'a03d71c5a85dfe7fd4c6d810b5f1f28884acc62685784ad3dad626ee558db71c'
+const charleyHexDiscoveryKey = '9dd05e8210a71cd195e349c5c0fcbf4d68ec96b5596474c6b3767ba4db3f64e2'
 
+// Corestore is a Hypercore factory
 // create a corestore instance with the given location
 const corestore = new Corestore('./reader-storage')
 
+// Hyperswarm allows you to find and connect to peers announcing a common 'topic'
 const hyperswarm = new Hyperswarm()
 goodbye(() => hyperswarm.destroy())
 
-// replication of the corestore instance on connection with other peers
+// Emitted whenever the swarm connects to a new peer
+// Replication of the corestore instance on connection with other peers
 hyperswarm.on('connection', (conn) => corestore.replicate(conn))
 
-// creation of Hypercore instance (if not already created)
+// Hypercore is a secure, distributed append-only log built for sharing large datasets and streams of real-time data.
+// Loads a Hypercore, either by name (if the name option is provided),
+// or from the provided key (if the first argument is a Buffer, or if the key option is set).
 const hypercore = corestore.get({ key: b4a.from(charleyHexDiscoveryKey, 'hex') })
 await hypercore.ready()
 
-// handshake with peers, but don't wait
+// flush() will wait until *all* discoverable peers have been connected to
+// It might take a while, so don't await it
+// Instead, use core.findingPeers() to mark when the discovery process is completed
 const foundPeers = corestore.findingPeers()
 hyperswarm.join(hypercore.discoveryKey)
 hyperswarm.flush().then(() => foundPeers())
 
-// update the meta-data information of the hypercore instance
+// Wait for the core to try and find a signed update to its length
+// Does not download any data from peers except for proof of the new core length
 await hypercore.update()
 
+// Hyperbee is an append-only B-tree based on Hypercore
 const hyperbee = new Hyperbee(hypercore, {
   keyEncoding: 'utf-8',
   valueEncoding: 'utf-8',
 })
 
-const logReadStream = async () => {
-  const db = {}
-  for await (const { key, value } of hyperbee.createReadStream()) {
-    db[key?.trim()] = value?.trim()
-  }
-  console.log({ db })
-}
-
-let version = hyperbee.version
+let version = 0
 console.log(hyperbee.version)
 setInterval(() => {
   if (hyperbee.version > version) {
@@ -55,28 +56,10 @@ setInterval(() => {
   }
 }, 500)
 
-// process.stdin.setEncoding('utf-8')
-// process.stdin.on('data', async (data) => {
-//   // const dynamic = await bee.get('dynamic')
-
-//   // console.log({ dynamic })
-
-//   const seq = hypercore1.length - 1
-//   const lastBlock = await hypercore1.get(hypercore1.length - 1)
-//   const { index, key, value } = Node.decode(lastBlock)
-//   const dIndex = Node.decode(index)
-//   // const dKey = Node.decode(key)
-//   // const dValue = Node.decode(value)
-
-//   // ​
-//   // // print the information about the last block or the latest block of the hypercore instance
-//   console.log({
-//     seq,
-//     index,
-//     key,
-//     value,
-//     dIndex,
-//     // dKey,
-//     // dValue,
-//   })
-// })
+async function logReadStream() {
+  const db = {}
+  for await (const { key, value } of hyperbee.createReadStream()) {
+    db[key?.trim()] = value?.trim()
+  }
+  console.log({ db })
+}
